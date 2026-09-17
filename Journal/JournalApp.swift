a9661622f77ffaf309ab63e61fabd72552e38c17
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-09-16 17:41:08 -0700

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Christian-SDGKU on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
